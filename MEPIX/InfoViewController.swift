03940e67f7afc0bef009ab8572b5ce80cc0dc498
d <anisenko.95@mail.ru>
2020-11-02 23:17:27 +0300

    Initial Commit

---
+++ MEPIX/InfoViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MEPIX
+//
+//  Created by Дима Анисенко on 21.02.2020.
+//  Copyright © 2020 Дима Анисенко. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    @IBOutlet weak var NewsTablleView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+   
+
+}
