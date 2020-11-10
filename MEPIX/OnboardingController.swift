5102fd75cdc8c095b30c01661059f3f2310442c5
d <anisenko.95@mail.ru>
2020-11-10 19:06:37 +0300

    Новый дезаин

---
+++ MEPIX/OnboardingController.swift
@@ -0,0 +1,22 @@
+//
+//  OnboardingController.swift
+//  MEPIX
+//
+//  Created by Дима Анисенко on 09.11.2020.
+//  Copyright © 2020 Дима Анисенко. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+ 
+
+}
