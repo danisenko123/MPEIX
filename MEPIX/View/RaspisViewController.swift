a9911f36983da1e4179158b778194bdb88aed90d
d <anisenko.95@mail.ru>
2021-01-06 19:02:08 +0300

    Создание модели для передачи меток карт.

--- MEPIX/View/RaspisViewController.swift
+++ MEPIX/View/RaspisViewController.swift
@@ -13,7 +13,7 @@ class RaspisViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+
     }
     
 
