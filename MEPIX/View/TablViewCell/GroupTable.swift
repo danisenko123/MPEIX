0aa370990bb019b34db8183cd816693fff331ade
d <anisenko.95@mail.ru>
2021-04-26 23:24:35 +0300

    Добавил UITableViewCell

---
+++ MEPIX/View/TablViewCell/GroupTable.swift
@@ -0,0 +1,19 @@
+//
+//  GroupTable.swift
+//  MEPIX
+//
+//  Created by Дима Анисенко on 26.04.2021.
+//  Copyright © 2021 Дима Анисенко. All rights reserved.
+//
+
+import UIKit
+
+class GroupTable: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+}
