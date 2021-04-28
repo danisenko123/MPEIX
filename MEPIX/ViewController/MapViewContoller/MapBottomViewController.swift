7d47bd9cfcddcd06d361c039167dc3ed8947dec7
Ivan Morozov <i.morozov@qsoft.ru>
2021-04-28 19:34:18 +0300

    Сверстал карту и нижнюю панель для нее.

---
+++ MEPIX/ViewController/MapViewContoller/MapBottomViewController.swift
@@ -0,0 +1,46 @@
+//
+//  MapBottomViewController.swift
+//  MEPIX
+//
+//  Created by Морозов Иван on 28.04.2021.
+//  Copyright © 2021 Дима Анисенко. All rights reserved.
+//
+
+import UIKit
+
+class MapBottomViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet var buttons: [UIButton]!
+    
+    var arrName = ["food","buildings","dorms","place","departments"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setNames()
+        tableView.dataSource = self
+    }
+    
+
+    func setNames(){
+        for (index, button) in buttons.enumerated(){
+            button.setImage(UIImage(named: arrName[index]), for: .normal)
+//            button.
+        }
+    }
+
+}
+
+extension MapBottomViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
+    
+    
+}
--- Podfile
+++ Podfile
@@ -7,5 +7,7 @@ target 'MEPIX' do
 
   pod 'Alamofire'
   pod 'ObjectMapper'
+  pod 'FloatingPanel'
+  pod 'SnapKit'
   
 end
