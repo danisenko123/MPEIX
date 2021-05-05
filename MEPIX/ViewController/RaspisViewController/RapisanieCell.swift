72aa574978b38108b37b5faf10093a9f92a8ec2f
d <anisenko.95@mail.ru>
2021-05-05 20:49:55 +0300

    Добавил новый класс, сверстал новый экран, доработал главный экранн

---
+++ MEPIX/ViewController/RaspisViewController/RapisanieCell.swift
@@ -0,0 +1,20 @@
+//
+//  RapisanieCell.swift
+//  MEPIX
+//
+//  Created by Дима Анисенко on 05.05.2021.
+//  Copyright © 2021 Дима Анисенко. All rights reserved.
+//
+
+import UIKit
+
+class RaspisCell: UITableViewCell {
+    
+    @IBOutlet weak var start: UILabel!
+    @IBOutlet weak var end: UILabel!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var place: UILabel!
+    @IBOutlet weak var person: UILabel!
+    @IBOutlet weak var number: UILabel!
+    @IBOutlet weak var rawType: UILabel!
+}
---
+++ MEPIX/ViewController/RaspisViewController/RaspisViewController.swift
@@ -0,0 +1,27 @@
+//
+//  RaspisViewController.swift
+//  MEPIX
+//
+//  Created by Дима Анисенко on 24.12.2020.
+//  Copyright © 2020 Дима Анисенко. All rights reserved.
+//
+
+import UIKit
+
+class RaspisViewController: UIViewController {
+    let networkManager = NetworkManager.shared
+    
+    @IBOutlet weak var raspistab: UITableView!
+    
+    var raspis: [Classes] = []
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
+
